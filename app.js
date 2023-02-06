87dbfd030e363b6cadd36a4ddad44db996a7fc70
Saray1606 <123569995+Saray1606@users.noreply.github.com>
2023-02-06 12:06:55 -0500

    crear clase

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "vaca",
+      img: "img/vaca.jpg"
+    },
+    {
+      name: "perro",
+      img: "img/perro.jpg"
+    }
+    {
+      name: "cabra",
+      img: "img/cabra.jpg"
+    }
+    {
+      name: "elefante",
+      img: "img/elefante.jpg"
+    }
+    {
+      name: "foca",
+      img: "img/foca.jpg"
+    }
+    {
+      name: "panda",
+      img: "img/panda.jpg"
+    }
+    
+  ];
+});
